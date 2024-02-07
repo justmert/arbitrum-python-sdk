6fbb926b48369e27ecb5a1a1d2e0fe513ec3de10
Mert Köklü <kklumert@gmail.com>
2024-02-07 19:59:19 +0300

    passing eth_bridge_adresses_test.py

--- tests/integration/eth_bridge_addresses_test.py
+++ tests/integration/eth_bridge_addresses_test.py
@@ -1,6 +1,6 @@
 import os
-import pytest
 from web3 import Web3
+from web3 import HTTPProvider
 from dotenv import load_dotenv
 
 # Assuming the functions `get_eth_bridge_information` and `get_l2_network` are implemented in your Python code
@@ -11,12 +11,10 @@ load_dotenv()
 
 def test_obtain_deployed_bridge_addresses():
     arb_one_l2_network = get_l2_network(42161)
-    eth_provider = Web3(Web3.HTTPProvider(os.getenv("MAINNET_RPC")))
+    eth_provider = Web3(HTTPProvider(os.getenv("MAINNET_RPC")))
 
-    # Obtain on-chain information
     eth_bridge = get_eth_bridge_information(arb_one_l2_network.eth_bridge.rollup, eth_provider)
 
-    # Assertions
     assert arb_one_l2_network.eth_bridge.bridge == eth_bridge.bridge, "Bridge contract is not correct"
     assert arb_one_l2_network.eth_bridge.inbox == eth_bridge.inbox, "Inbox contract is not correct"
     assert (
